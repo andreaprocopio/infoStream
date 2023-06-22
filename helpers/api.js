d53cf6ca9edbd6fe813329d6c1cb6d4520d819ec
andreaprocopio <andreproco@gmail.com>
2023-06-22 21:24:28 +0200

    added upcoming movies and fixed trending

--- helpers/api.js
+++ helpers/api.js
@@ -1,5 +1,5 @@
 export async function getTrendingMovies() {
-  const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+  const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
   const options = {
     method: 'GET',
     headers: {
@@ -42,6 +42,29 @@ export async function getTrendingTv() {
     const data = await res.json()
     return data
 
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+export async function getUpcomingMovies() {
+  const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1'
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGE1Yzk1YzlkOGQ3MWUzMDE4ODM4NjNjZThiOTU4MCIsInN1YiI6IjY0OGY3NDRhNDJiZjAxMDBhZTMyMTY5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._i5kqlYKtgpwNppB3bMgY2S5WvbBR1teOrWNR6MiuGE'
+    }
+  };
+
+  try {
+    const res = await fetch(url, options);
+
+    if (!res.ok) {
+      throw new Error('Could not fetch Upcoming movies')
+    }
+    const data = await res.json()
+    return data
   } catch(error) {
     console.error(error)
   }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Footer from "@/components/Footer";
 import MovieCarousel from "@/components/MovieCarousel";
-import { getTrendingMovies, getTrendingTv } from "@/helpers/api";
+import { getTrendingMovies, getTrendingTv, getUpcomingMovies } from "@/helpers/api";
 import Hero from "@/components/Hero";
 
 export default function Homepage(props) {
 
   const trendingMovies = props.trendingMovies
   const trendingTv = props.trendingTv
+  const upcomingMovies = props.upcomingMovies
 
   return (
     <>
       <Hero />
       <MovieCarousel color={0} content={'Trending movies:'} movies={trendingMovies.results} />
-      <MovieCarousel color={900} content={'Trending Tv'} movies={trendingTv.results} />
+      <MovieCarousel color={900} content={'Trending Tv:'} movies={trendingTv.results} />
+      <MovieCarousel color={0} content={'Upcoming Movies:'} movies={upcomingMovies.results} />
       <Footer />
     </>
   )
@@ -21,11 +23,13 @@ export default function Homepage(props) {
 export async function getStaticProps() {
   const trendingMovies = await getTrendingMovies()
   const trendingTv = await getTrendingTv()
+  const upcomingMovies = await getUpcomingMovies()
 
   return {
     props: {
       trendingMovies,
-      trendingTv
+      trendingTv,
+      upcomingMovies
     },
     revalidate: 900 //15 m
   }
