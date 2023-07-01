ca18f6f2ef35835d9250acba206efea424aeb86a
andreaprocopio <andreproco@gmail.com>
2023-07-01 11:27:53 +0200

    changed small card button

--- components/CarouselCardSmall.jsx
+++ components/CarouselCardSmall.jsx
@@ -26,7 +26,7 @@ const CarouselCardSmall = (props) => {
           <AiOutlineStar size={15} className='text-amber-400'/>
         </div>
       </div>
-      <button className="absolute right-2 bottom-2 inline-flex items-center justify-center overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 z-[10001]">
+      <button className="absolute right-2 bottom-2 inline-flex items-center justify-center overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:ring-0 focus:outline-none z-[10001]">
         <span className="relative px-2.5 py-1 transition-all ease-in duration-75 rounded-md group-hover:bg-opacity-0">
           Upcoming
         </span>
